55bb1df386618992ecb097497885535ac1a58447
Ulises Flores <ulises.fg.98@gmail.com>
2023-10-13 22:02:37 -0600

    add function2

--- s1variablesFunciones/main.js
+++ s1variablesFunciones/main.js
@@ -1,4 +1,4 @@
-console.log("hola koders")
+/*console.log("hola koders")
 
 let numero1 = 10;
 let numero2 = 5;
@@ -11,4 +11,25 @@ let division = numero1 / numero2;
 console.log("Suma: " + suma);
 console.log("Resta: " + resta);
 console.log("Multiplicación: " + multiplicacion);
-console.log("División: " + division);
+console.log("División: " + division);*/
+
+function esParOImpar(numero) {
+    if (numero % 2 === 0) {
+      return "Par";
+    } else {
+      return "Impar";
+    }
+  }
+  
+  let numeroIngresado = prompt("Por favor, ingrese un número:");
+  let numero = parseInt(numeroIngresado);
+  
+  if (!isNaN(numero))
+   { 
+    let resultado = esParOImpar(numero);
+    console.log(`El número ${numero} es ${resultado}.`);
+  } 
+  else {
+    console.log("Por favor, ingrese un número válido.");
+  }
+  
